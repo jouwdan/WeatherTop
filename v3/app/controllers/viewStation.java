353275b0ff9218749b7b1524c39d6e17c3c44c6e
Jordan Harrison <76656637+jordanleeharrison@users.noreply.github.com>
2021-04-27 21:00:08 +0100

    add reading form added

--- v3/app/controllers/viewStation.java
+++ v3/app/controllers/viewStation.java
@@ -13,4 +13,12 @@ public class viewStation extends Controller {
         Logger.info("Station id = " + id);
         render ("station.html", station);
     }
+
+    public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure) {
+        Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
+        Station station = Station.findById(id);
+        station.readings.add(reading);
+        station.save();
+        redirect("/station/" + id);
+    }
 }
