bf15fbde470dc08988dd609616a55df0969598b7
Jordan Harrison <contact@jordanharrison.ie>
2021-04-28 14:11:40 +0100

    Version 2 Release

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -8,10 +8,12 @@ public class Reading extends Model {
     public int code;
     public double temperature;
     public double windSpeed;
+    public int pressure;
 
-    public Reading(int code, double temperature, double windSpeed) {
+    public Reading(int code, double temperature, double windSpeed, int pressure) {
         this.code = code;
         this.temperature = temperature;
         this.windSpeed = windSpeed;
+        this.pressure = pressure;
     }
 }
