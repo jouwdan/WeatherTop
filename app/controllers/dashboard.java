1bc74f42ba8c903df626079420d9b0e76467965b
Jordan Harrison <contact@jordanharrison.ie>
2021-04-28 14:13:37 +0100

    Version 3

--- app/controllers/dashboard.java
+++ app/controllers/dashboard.java
@@ -16,4 +16,11 @@ public class dashboard extends Controller {
         render ("dashboard.html", stations);
     }
 
+    public static void addStation(String name, double lat, double lng) {
+        Station station = new Station(name, lat, lng);
+        Logger.info ("Adding a new station called " + name);
+        station.save();
+        redirect ("/dashboard");
+    }
+
 }
--- app/models/Reading.java
+++ app/models/Reading.java
@@ -8,12 +8,14 @@ public class Reading extends Model {
     public int code;
     public double temperature;
     public double windSpeed;
+    public double windDirection;
     public int pressure;
-
-    public Reading(int code, double temperature, double windSpeed, int pressure) {
+    
+    public Reading(int code, double temperature, double windSpeed, double windDirection, int pressure) {
         this.code = code;
         this.temperature = temperature;
         this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
         this.pressure = pressure;
     }
 }
