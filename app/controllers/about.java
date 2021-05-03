8a7ab26a3419588eb74454c153db925f6800e58b
Jordan Harrison <76656637+jordanleeharrison@users.noreply.github.com>
2021-05-03 12:56:28 +0100

    Nav dropdown added for logged in user

--- app/controllers/about.java
+++ app/controllers/about.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import models.Member;
 import play.Logger;import play.mvc.Controller;
 
 public class about extends Controller {
     public static void index() {
+        Member member = accounts.getLoggedInMember();
         Logger.info("Rendering About");
         render ("about.html");
     }
--- app/controllers/home.java
+++ app/controllers/home.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import models.Member;
 import play.Logger;import play.mvc.Controller;
 
 public class home extends Controller {
     public static void index() {
+        Member member = accounts.getLoggedInMember();
         Logger.info("Rendering Home");
         render ("home.html");
     }
