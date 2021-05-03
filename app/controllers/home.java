3c544022907d8c23f6ecbddb6a24e918a087545b
Jordan Harrison <76656637+jordanleeharrison@users.noreply.github.com>
2021-05-03 19:46:27 +0100

    delete reading functionality added

--- app/controllers/home.java
+++ app/controllers/home.java
@@ -1,7 +1,8 @@
 package controllers;
 
 import models.Member;
-import play.Logger;import play.mvc.Controller;
+import play.Logger;
+import play.mvc.Controller;
 
 public class home extends Controller {
     public static void index() {
---
+++ app/controllers/stationControl.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Station;
+import models.Reading;
+
+import play.Logger;
+import play.mvc.Controller;
+
+public class stationControl extends Controller {
+    public static void deleteReading (Long id, Long readingid) {
+        Station station = Station.findById(id);
+        Reading reading = Reading.findById(readingid);
+        Logger.info ("Deleting Reading: " + readingid);
+        station.readings.remove(reading);
+        station.save();
+        reading.delete();
+        render("station.html", station);
+    }
+}
