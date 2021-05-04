00be8e951bbcba423a45d0938b7806a2c63ab1e5
Jordan Harrison <contact@jordanharrison.ie>
2021-05-04 09:42:13 +0100

    login requirement removed from home & about pages

--- app/controllers/about.java
+++ app/controllers/about.java
@@ -5,7 +5,6 @@ import play.Logger;import play.mvc.Controller;
 
 public class about extends Controller {
     public static void index() {
-        Member member = accounts.getLoggedInMember();
         Logger.info("Rendering About");
         render ("about.html");
     }
--- app/controllers/home.java
+++ app/controllers/home.java
@@ -6,7 +6,6 @@ import play.mvc.Controller;
 
 public class home extends Controller {
     public static void index() {
-        Member member = accounts.getLoggedInMember();
         Logger.info("Rendering Home");
         render ("home.html");
     }
