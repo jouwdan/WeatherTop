533c6915a6ee2d1cffda1817d0b5e4203df60bdc
Jordan Harrison <76656637+jordanleeharrison@users.noreply.github.com>
2021-05-06 19:34:34 +0100

    prod

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -11,6 +11,8 @@ public class Bootstrap extends Job
 { 
   public void doJob()
   {
-    Fixtures.loadModels("data.yml");
+    if(Member.count() == 0) {
+      Fixtures.loadModels("data.yml");
+    }
   }
 }
