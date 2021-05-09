5980108844fb016bf76744061852ae85bc8ce5d5
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-05-09 20:31:39 +0100

    alphabetical sorting

--- app/controllers/dashboard.java
+++ app/controllers/dashboard.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 import models.Station;
 import models.Reading;
@@ -16,6 +18,16 @@ public class dashboard extends Controller {
         Logger.info("Rendering Dashboard");
         Member member = accounts.getLoggedInMember();
         List<Station> stations = member.stations;
+
+        // stackoverflow link for this fix: https://stackoverflow.com/a/44096027
+        Collections.sort(stations, new Comparator<Station>() {
+            public int compare(Station station, Station t1) {
+                String s1 = station.name;
+                String s2 = t1.name;
+                return s1.compareToIgnoreCase(s2);
+            }
+        });
+
         for (Station station:member.stations) {
             WeatherUtility.updateWeather(station);
         }
